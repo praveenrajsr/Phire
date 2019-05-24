4152ad89ea2b0f2e8ee0e56535780d5d26e69b85
praveenraj <aashooshome@gmail.com>
2019-05-24 07:15:59 +0530

    Add files via upload

--- Phire/js/main.js
+++ Phire/js/main.js
@@ -1,62 +1,56 @@
-$(document).ready(()=>{
-    
-    $termsandconditions = $('.termsandconditionswrapper');
-    $question_container = $('#fullcontainer');
-    $('#fullcontainer').hide();
-    $('#next-btn').on('click', ()=>{
-        $termsandconditions.toggleClass('hide');
-        $('#fullcontainer').show();
-        $('#time-section').timeTo(2400, function(){
-            alert("Count down Finished!");
-        });  
-    });   
+$(document).ready(() => {
     $currentdiv = $('#start');
     $submit_btn = $('#submit-btn');
-    if($currentdiv.attr('data-order') == 1){
+    if ($currentdiv.attr('data-order') == 1) {
         $('.previous-btn').addClass('hide');
-    }else{
+    } else {
         $('.previous-btn').removeClass('hide');
     }
-    $('.next-btn').on('click', (e)=>{
+    $('#next-btn').on('click', () => {
+        $('#time-section').timeTo(2400, function () {
+            alert("Count down Finished!");
+        });
+    });
+    $('.next-btn').on('click', (e) => {
         e.preventDefault();
         $currentdiv.toggleClass('hide');
         $currentdiv = $currentdiv.next()
-        ;
+            ;
         $currentdiv.toggleClass('hide');
-        if($currentdiv.attr('data-order') == 3){
+        if ($currentdiv.attr('data-order') == 3) {
             $('.next-btn').addClass('hide');
-        }else{
+        } else {
             $('.next-btn').removeClass('hide');
         }
-        if($currentdiv.attr('data-order') == 1){
+        if ($currentdiv.attr('data-order') == 1) {
             $('.previous-btn').addClass('hide');
-        }else{
+        } else {
             $('.previous-btn').removeClass('hide');
         }
-        if($currentdiv.attr('data-order') == 3){
+        if ($currentdiv.attr('data-order') == 3) {
             $submit_btn.removeClass('hide')
         }
     });
-    $('.previous-btn').on('click', (e)=>{
-       e.preventDefault();
-       $currentdiv.toggleClass('hide');
-       $currentdiv = $currentdiv.prev();
-       $currentdiv.toggleClass('hide');
-       if($currentdiv.attr('data-order') == 3){
+    $('.previous-btn').on('click', (e) => {
+        e.preventDefault();
+        $currentdiv.toggleClass('hide');
+        $currentdiv = $currentdiv.prev();
+        $currentdiv.toggleClass('hide');
+        if ($currentdiv.attr('data-order') == 3) {
             $('.next-btn').addClass('hide');
-        }else{
+        } else {
             $('.next-btn').removeClass('hide');
         }
-       if($currentdiv.attr('data-order') == 1){
+        if ($currentdiv.attr('data-order') == 1) {
             $('.previous-btn').addClass('hide');
-        }else{
+        } else {
             $('.previous-btn').removeClass('hide');
         }
-        if($currentdiv.attr('data-order') == 3){
+        if ($currentdiv.attr('data-order') == 3) {
             $submit_btn.removeClass('hide')
         }
     });
-    $submit_btn.on('click',()=>{
+    $submit_btn.on('click', () => {
         alert("Thank you for submitting!");
     })
 });
