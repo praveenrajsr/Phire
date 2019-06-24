f5b13e5530594ee8cfc077feddb7bdb05fee14a3
praveenraj <aashooshome@gmail.com>
2019-06-24 07:57:33 +0530

    Add files via upload

--- online_question/js/main.js
+++ online_question/js/main.js
@@ -1,57 +1,68 @@
+$currentdiv = $('#current');
+$next = $('.next');
 $(document).ready(() => {
     count = 1;
     $questions = $('.questions');
     for (i = 1; i <= $questions.length; i++) {
-        $('.question-num').append("<li class='each-ques-num' data-num=" + i + "  onClick='attribute(this)'>" + i + "</li>");
+        $('.question-num').append("<li class='each-ques-num " + i + "' data-num=" + i + "  onClick='attribute(this)'>" + i + "</li>");
         // $questions.attr('data-quesnum', i);
     }
-    $currentdiv = $('#current');
     for (j = 1; j <= $questions.length; j++) {
         $currentdiv.attr('data-qnum', j);
         $currentdiv = $currentdiv.next();
     }
-    $next = $('.next');
     $current = $('#current')
-    j = 1
     $next.on('click', (e) => {
         e.preventDefault();
+        current_ques_num = $current.attr('data-qnum');
+        current_name = $current.children().find('input').attr('name');
+
+        if ($("input[name=" + current_name + "]").is(":checked")) {
+            console.log($current.attr('data-qnum'))
+            $("." + current_ques_num).addClass('green');
+        }
         $current.toggleClass("hide");
         $current = $current.next();
         $current.toggleClass("hide");
-        j = j + 1
+        j = $current.attr('data-qnum');
         // console.log(j)
         if (j == $questions.length) {
             $next.css('display', 'none');
+        } else {
+            $next.css('display', 'block');
         }
     });
 
 });
 function attribute(data) {
     jumpnumber = $(data).data('num');
-    console.log(jumpnumber);
-    $current.toggleClass('hide');
-    if ($current.attr('data-qnum') > jumpnumber) {
-        inbetween = $current.attr('data-qnum') - jumpnumber;
-        for (i = $current.attr('data-qnum'); i < jumpnumber; i++) {
-            if ($('.each-ques-num').attr('data-num') == i) {
-                $('.each-ques-num').addClass('yellow');
-            }
-            $('.each-ques-num').next();
-        }
+    initial_ques = 1
+    current_ques_num = $('.active').attr('data-qnum');
+    current_name = $current.children().find('input').attr('name');
 
-    } else {
-
+    if ($("input[name=" + current_name + "]").is(":checked")) {
+        $("." + current_ques_num).addClass('green');
     }
+    $current.toggleClass('hide');
+    $current.removeClass('active');
+
     while ($current.attr('data-qnum') != jumpnumber) {
         if ($current.attr('data-qnum') > jumpnumber) {
             $current = $current.prev();
         } else if ($current.attr('data-qnum') < jumpnumber) {
             $current = $current.next();
-
         } else {
 
         }
 
     }
     $current.toggleClass('hide');
+    $current.addClass('active');
+    j = $current.attr('data-qnum');
+    // console.log(j)
+    if (j == $questions.length) {
+        $next.css('display', 'none');
+    } else {
+        $next.css('display', 'block');
+    }
 }
